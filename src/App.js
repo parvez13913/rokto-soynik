096678c8bb18930893b7af6a5a42e756d8e94889
parvez13913 <parvezz13913@gmail.com>
2022-08-08 16:24:53 -0700

    Header set complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Home from './Pages/Home/Home';
+import Header from './Pages/Shared/Header/Header';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
